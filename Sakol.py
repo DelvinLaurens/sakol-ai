fab1ecc71abe460ad1ae2aa1d3b1fd6c51275b37
DelvinLaurens <delvinlaurens6@gmail.com>
2026-03-07 02:27:41 +0700

    perbaikin requiment

--- Sakol.py
+++ Sakol.py
@@ -1,22 +1,21 @@
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 import os
 from groq import Groq
 
-load_dotenv("secret.env")
-
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+# Ambil token dari Environment variable
+GROQ_API_KEY = os.environ['GROQ_API_KEY']
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 
 print("Groq:", GROQ_API_KEY)
 print("Discord:", DISCORD_TOKEN)
 
-client = Groq(api_key=GROQ_API_KEY)
+# Client Groq untuk AI
+groq_client = Groq(api_key=GROQ_API_KEY)
 
+# Bot Discord
 intents = discord.Intents.default()
 intents.message_content = True
-
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -25,16 +24,11 @@ async def on_ready():
 
 @bot.command()
 async def hallo(ctx, *, prompt):
-
-    response = client.chat.completions.create(
+    response = groq_client.chat.completions.create(
         model="llama-3.1-8b-instant",
-        messages=[
-            {"role": "user", "content": prompt}
-        ]
+        messages=[{"role": "user", "content": prompt}]
     )
-
     reply = response.choices[0].message.content
     await ctx.send(reply)
 
 bot.run(DISCORD_TOKEN)
-
